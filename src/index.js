7042455afa5face26f9117f6a55a5e12011c1f8e
krestine <krestine@nate.com>
2022-01-12 14:38:19 +0900

    CSS update & Bug fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import "./style.css";
+import "./index.css";
 
 class App extends React.Component {
   state = {
@@ -10,10 +10,23 @@ class App extends React.Component {
     Def: "",
     Eva: "",
     Res: "",
-    RealCri: 0,
-    RealAcc: 0,
-    RealDef: 0,
-    MinDam: 0
+    RealCri: "",
+    RealAcc: "",
+    RealDef: "",
+    MinDam: "",
+    Language: "Kor",
+    ResetString: {"Kor":"초기화", "Eng":"Reset"},
+    CalculateString: {"Kor":"계산하기", "Eng":"Calculate"},
+    CriString: {"Kor":"치명타", "Eng":"Critical"},
+    AccString: {"Kor":"명중", "Eng":"Accuracy"},
+    StaString: {"Kor":"안정성", "Eng":"Stability"},
+    DefString: {"Kor":"방어력", "Eng":"Defence"},
+    EvaString: {"Kor":"회피", "Eng":"Evasion"},
+    ResString: {"Kor":"치명타 저항", "Eng":"Crit Resistance"},
+    RealCriString: {"Kor":"실제 치명타 확률 : ", "Eng":"Critical Rate : "},
+    RealAccString: {"Kor":"실제 명중률 : ", "Eng":"Accuracy Rate : "},
+    RealDefString: {"Kor":"실제 받는 피해 : ", "Eng":"Damage Taken : "},
+    MinDamString: {"Kor":"대미지 범위 : ", "Eng":"Dam. Variance : "}
   };
 
   safeParseFloat = (str) => {
@@ -36,12 +49,13 @@ class App extends React.Component {
   };
 
   calculate = () => {
-    //const { Cri, Acc } = this.state;
-    //alert("The SUM is " + (Cri + Acc));
     var tempCri = this.state.Cri - this.state.Res;
     var finalCri = (tempCri * 6000) / (tempCri * 6000 + 4000000);
+	if(tempCri < 0){
+		finalCri = 0;
+	}
     this.setState({ RealCri: (finalCri * 100).toFixed(3) });
-    var finalDef = 1 / (1 + this.state.Def / 1666.66);
+    var finalDef = 1 / (1 + this.state.Def / 1666.667);
     this.setState({ RealDef: (finalDef * 100).toFixed(3) });
     var tempAcc = this.state.Eva - this.state.Acc;
     var finalAcc = 0;
@@ -62,66 +76,98 @@ class App extends React.Component {
     this.setState({ Def: "" });
     this.setState({ Eva: "" });
     this.setState({ Res: "" });
+    this.setState({ RealCri: "" });
+    this.setState({ RealAcc: "" });
+    this.setState({ RealDef: "" });
+    this.setState({ MinDam: "" });
+  };
+
+  LanguageChange = (e) => {
+    this.setState({
+      Language: e.target.value
+    });
   };
 
   render() {
     return (
       <div>
-        <label>치명타</label>
+        <label>{this.state.CriString[this.state.Language]}</label>
         <input
-          placeholder="공격자 치명타"
+          //placeholder="공격자 치명타"
           value={this.state.Cri}
           name="Cri"
           onChange={(evt) => this.updateInputValue(evt)}
         />
-        <label>명중</label>
+        <label>{this.state.AccString[this.state.Language]}</label>
         <input
-          placeholder="공격자 명중"
+          //placeholder="공격자 명중"
           value={this.state.Acc}
           name="Acc"
           onChange={(evt) => this.updateInputValue(evt)}
         />
-        <label>안정성</label>
+        <label>{this.state.StaString[this.state.Language]}</label>
         <input
-          placeholder="공격자 안정성"
+          //placeholder="공격자 안정성"
           value={this.state.Sta}
           name="Sta"
           onChange={(evt) => this.updateInputValue(evt)}
         />
-        <br></br>
-        <label>방어력</label>
+        <br/><br/>
+        <label>{this.state.DefString[this.state.Language]}</label>
         <input
-          placeholder="방어자 방어력"
+          //placeholder="방어자 방어력"
           value={this.state.Def}
           name="Def"
           onChange={(evt) => this.updateInputValue(evt)}
         />
-        <label>회피</label>
+        <label>{this.state.EvaString[this.state.Language]}</label>
         <input
-          placeholder="방어자 회피"
+          //placeholder="방어자 회피"
           value={this.state.Eva}
           name="Eva"
           onChange={(evt) => this.updateInputValue(evt)}
         />
-        <label>치명타 저항</label>
+        <label>{this.state.ResString[this.state.Language]}</label>
         <input
-          placeholder="방어자 치명타 저항"
+          //placeholder="방어자 치명타 저항"
           value={this.state.Res}
           name="Res"
           onChange={(evt) => this.updateInputValue(evt)}
         />
-        <br></br>
-        <button onClick={this.calculate}>계산하기</button>
-        <button onClick={this.resetAll}>초기화</button>
+        <br/><br/>
+        <button onClick={this.calculate}>{this.state.CalculateString[this.state.Language]}</button>
+        <button onClick={this.resetAll}>{this.state.ResetString[this.state.Language]}</button>
+		<br/><br/>
         <div>
           <text name="result" id="result">
-            <div>실제 치명타 확률 : {this.state.RealCri} %</div>
-            <div>실제 명중률 : {this.state.RealAcc} %</div>
-            <div>실제 받는 피해 : {this.state.RealDef} %</div>
-            <div>대미지 범위 : {this.state.MinDam} ~ 100%</div>
+            <div>{this.state.RealCriString[this.state.Language]}{this.state.RealCri} %</div>
+            <div>{this.state.RealAccString[this.state.Language]}{this.state.RealAcc} %</div>
+            <div>{this.state.RealDefString[this.state.Language]}{this.state.RealDef} %</div>
+            <div>{this.state.MinDamString[this.state.Language]}{this.state.MinDam} - 100%</div>
           </text>
         </div>
-      </div>
+        <br/>
+		<div align="center">
+			<input
+			  id="Korean"
+			  value="Kor"
+			  name="language"
+			  type="radio"
+			  checked={this.state.Language === "Kor"}
+			  onChange={this.LanguageChange}
+			/>
+			한국어 　
+			<input
+			  id="English"
+			  value="Eng"
+			  name="language"
+			  type="radio"
+			  checked={this.state.Language === "Eng"}
+			  onChange={this.LanguageChange}
+			/>
+			English
+			</div>
+        </div>
     );
   }
 }
